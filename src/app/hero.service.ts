0bd10349a80cbd05de2d254c883355070be6588b
Ashish Ojha <ashish.ojha@xoriant.com>
2019-09-23 14:12:49 +0530

    Added more components like search

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -72,5 +72,34 @@ export class HeroService {
       );
   }
 
+  // POST: add a new Hero to the server
+  addHero(hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap( (newHero: Hero) => this.log(`added hero id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<any> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id = ${id}`) ),
+        catchError(this.handleError<Hero>('deletedHero'))
+      )
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) return of([]);
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      )
+  }
+
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -29,6 +29,24 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes );
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return }
+    this.heroService.addHero( { name } as Hero )
+    .subscribe(
+      // (hero) => this.heroes.push(hero)
+    );
+  }
+
+  delete(hero: Hero): void {
+    // this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe( () => { 
+        console.log("subsSCRIBE"); 
+        this.heroes = this.heroes.filter(h => h !== hero);
+      }
+    );
+  }
+
   // onClick(hero: Hero): void {
   //   console.log("onC: H", hero);
   //   this.selectedHero = hero;
